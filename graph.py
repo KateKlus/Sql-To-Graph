176fc791e05b7bda5ac385d8290b2641b54c112a
KateKlus <ekaterina.klus@gmail.com>
2018-06-18 11:20:05 +0300

    Graph colored

--- graph.py
+++ graph.py
@@ -3,9 +3,8 @@ import matplotlib.pyplot as plt
 
 
 def draw_graph(graph, graph_layout='spring',
-               node_size=1600, node_color='blue', node_alpha=0.3,
-               node_text_size=12,
-               edge_color='blue', edge_alpha=0.3, edge_tickness=1,
+               node_size=1600, node_alpha=0.3,
+               node_text_size=12, edge_alpha=0.3, edge_tickness=1,
                text_font='sans-serif'):
 
     # Создаем граф
@@ -25,13 +24,28 @@ def draw_graph(graph, graph_layout='spring',
     else:
         graph_pos = nx.shell_layout(g)
 
+    # Окрашиваем узлы в соответствии с типом
+    color_map = []
+    legend_map = []
+    for node in g:
+        if node == 'R':
+            color_map.append('red')
+            legend_map.append('Final result')
+        elif node[0] == 's':
+            color_map.append('blue')
+            legend_map.append('Subqueries')
+        else:
+            color_map.append('green')
+            legend_map.append('Tables')
+
     # Отрисовываем граф
     nx.draw_networkx_nodes(g, graph_pos, node_size=node_size,
-                           alpha=node_alpha, node_color=node_color)
+                           alpha=node_alpha, node_color=color_map)
     nx.draw_networkx_edges(g, graph_pos, width=edge_tickness,
-                           alpha=edge_alpha, edge_color=edge_color)
+                           alpha=edge_alpha, edge_color='black')
     nx.draw_networkx_labels(g, graph_pos, font_size=node_text_size,
                             font_family=text_font)
 
+    # TODO добавить отрисовку легенды
     # Показываем граф
     plt.show()
